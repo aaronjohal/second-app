6fbcc0f24662fb34a40d493291638a58a6b32e49
Aaron Johal <aaronjohal@gmail.com>
2020-04-08 15:34:51 +0100

    UI 50% complete

---
+++ window-shopper/window-shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,21 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by Aaron Johal on 08/04/2020.
+//  Copyright © 2020 Aaron Johal. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
